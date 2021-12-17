38deba225ee334461a99d98f0390f14c4f52fcdf
Gabriel Chagas <gabrielserq@outlook.com>
2021-12-16 22:04:05 -0300

    chore: using swaper to create a carousel.

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,7 +38,21 @@ function scrollHeader(){
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== NEW SWIPER ===============*/
-
+var newSwiper = new Swiper(".new-swiper", {
+    spaceBetween: 24,
+    loop: 'true',
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true
+    },
+    breakpoints: {
+        992: {
+          spaceBetween: 80,
+        },
+    },
+  });
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
